2656881c23a50b17df3971c44072e2295ad1eb24
IGraciarena <ivanigraciarena@hotmail.com>
2020-04-19 12:13:56 -0300

    deleting unusefull threads on main

--- 4 Cerveceria TP/src/com/company/Main.java
+++ 4 Cerveceria TP/src/com/company/Main.java
@@ -10,30 +10,11 @@ public class Main {
         //              si no hay tarta despierto al productor y me duermo .produzco
 
         BeerHouse beerHouse = new BeerHouse(60,3);
-
         BeerConsumer b1 = new BeerConsumer(beerHouse);
-        BeerConsumer b2 = new BeerConsumer(beerHouse);
-        BeerConsumer b3 = new BeerConsumer(beerHouse);
-        BeerConsumer b4 = new BeerConsumer(beerHouse);
-        BeerConsumer b5 = new BeerConsumer(beerHouse);
-
         BeerProducer bp1 = new BeerProducer(50,beerHouse);
+
         bp1.start();
         b1.start();
-        b2.start();
-        b3.start();
-        b4.start();
-        b5.start();
-
-        try {
-            b2.sleep(100000000);
-            b3.sleep(120000000);
-            b4.sleep(130000000);
-            b5.sleep(140000000);
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
 
     }
